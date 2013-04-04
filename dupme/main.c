098798a03ca73a3faeb9bfc0fc8ebc63510a9e9c
Toluk <Wapuk-cobaka@yandex.ru>
2013-04-04 23:50:49 +0400

    Update main.c

--- dupme/main.c
+++ dupme/main.c
@@ -4,10 +4,25 @@
 
 const int buff = 512;
 
+int i_want_atoi(char* why)
+    {
+        int temp = 0, i = 0;
+        while(why[i] >= '0' && why[i] <= '9')
+            {
+                temp = temp * 10 + why[i] - 48;
+                i++;
+            }
+        return temp;
+    }
+
 int main(int argc, char* argv[])
     {
-        int k = atoi(argv[1]), flag1 = 1, flag2 = 1, temp_pointer = 0, i = 0;
-        char *temp = (char *) malloc(k + 1), *buffer = (char *) malloc(buff);
+        int k = i_want_atoi(argv[1]), flag1 = 1, flag2 = 1, temp_pointer = 0, i = 0;
+        /*char *temp = (char *) malloc(k + 1), *buffer = (char *) malloc(buff);*/
+        /*дико странное поведение, если первая строка не влезает*/
+        /*ее она обрабатывает криво, а вот все последующие почемуто нормально*/
+        /*вариант, который ниже, работает прекрастно*/
+        char temp[k + 1], buffer[buff];
         while (flag1 == 1)
             {
                 int hmr = read(0, buffer, buff), i = 0;
